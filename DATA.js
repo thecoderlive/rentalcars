1bb8912c2fc0d2b53d5523021a43988b36bf9566
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-01 19:35:29 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,12 @@
+export const item = {
+    "id": "ca3b957b-1812-4cb0-8690-88f112797c5f",
+    "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png",
+    "main_title": "Notice grow their decade. Plant public phone start. None goal marriage campaign language.",
+    "insurance_text": "because",
+    "liberty_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png",
+    "liberty_mutual": "total",
+    "insurance": "mean",
+    "car_clip": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png",
+    "endless_option": "around",
+    "about_models": "Stand civil player music important agent. Tough bed fund pick local decade four."
+}
